a832e27a281b11e247b4ec430e4aff229c1fd9b5
Guido Salazar Vargas <81344239+GuidoSV7@users.noreply.github.com>
2025-07-26 16:34:16 -0400

    Add files via upload

---
+++ contract/SimpleDEX/SimpleDEX_flattened.sol
@@ -0,0 +1,295 @@
+
+// File: @openzeppelin/contracts/token/ERC20/IERC20.sol
+
+
+// OpenZeppelin Contracts (last updated v5.4.0) (token/ERC20/IERC20.sol)
+
+pragma solidity >=0.4.16;
+
+/**
+ * @dev Interface of the ERC-20 standard as defined in the ERC.
+ */
+interface IERC20 {
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /**
+     * @dev Returns the value of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the value of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves a `value` amount of tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 value) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets a `value` amount of tokens as the allowance of `spender` over the
+     * caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 value) external returns (bool);
+
+    /**
+     * @dev Moves a `value` amount of tokens from `from` to `to` using the
+     * allowance mechanism. `value` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+}
+
+// File: @openzeppelin/contracts/utils/ReentrancyGuard.sol
+
+
+// OpenZeppelin Contracts (last updated v5.1.0) (utils/ReentrancyGuard.sol)
+
+pragma solidity ^0.8.20;
+
+/**
+ * @dev Contract module that helps prevent reentrant calls to a function.
+ *
+ * Inheriting from `ReentrancyGuard` will make the {nonReentrant} modifier
+ * available, which can be applied to functions to make sure there are no nested
+ * (reentrant) calls to them.
+ *
+ * Note that because there is a single `nonReentrant` guard, functions marked as
+ * `nonReentrant` may not call one another. This can be worked around by making
+ * those functions `private`, and then adding `external` `nonReentrant` entry
+ * points to them.
+ *
+ * TIP: If EIP-1153 (transient storage) is available on the chain you're deploying at,
+ * consider using {ReentrancyGuardTransient} instead.
+ *
+ * TIP: If you would like to learn more about reentrancy and alternative ways
+ * to protect against it, check out our blog post
+ * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
+ */
+abstract contract ReentrancyGuard {
+    // Booleans are more expensive than uint256 or any type that takes up a full
+    // word because each write operation emits an extra SLOAD to first read the
+    // slot's contents, replace the bits taken up by the boolean, and then write
+    // back. This is the compiler's defense against contract upgrades and
+    // pointer aliasing, and it cannot be disabled.
+
+    // The values being non-zero value makes deployment a bit more expensive,
+    // but in exchange the refund on every call to nonReentrant will be lower in
+    // amount. Since refunds are capped to a percentage of the total
+    // transaction's gas, it is best to keep them low in cases like this one, to
+    // increase the likelihood of the full refund coming into effect.
+    uint256 private constant NOT_ENTERED = 1;
+    uint256 private constant ENTERED = 2;
+
+    uint256 private _status;
+
+    /**
+     * @dev Unauthorized reentrant call.
+     */
+    error ReentrancyGuardReentrantCall();
+
+    constructor() {
+        _status = NOT_ENTERED;
+    }
+
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and making it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        _nonReentrantBefore();
+        _;
+        _nonReentrantAfter();
+    }
+
+    function _nonReentrantBefore() private {
+        // On the first call to nonReentrant, _status will be NOT_ENTERED
+        if (_status == ENTERED) {
+            revert ReentrancyGuardReentrantCall();
+        }
+
+        // Any calls to nonReentrant after this point will fail
+        _status = ENTERED;
+    }
+
+    function _nonReentrantAfter() private {
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = NOT_ENTERED;
+    }
+
+    /**
+     * @dev Returns true if the reentrancy guard is currently set to "entered", which indicates there is a
+     * `nonReentrant` function in the call stack.
+     */
+    function _reentrancyGuardEntered() internal view returns (bool) {
+        return _status == ENTERED;
+    }
+}
+
+// File: contracts/SimpleDEX/SimpleDEX.sol
+
+
+pragma solidity ^0.8.20;
+
+contract SimpleDEX is ReentrancyGuard {
+    address public owner;
+    IERC20 public tokenA;
+    IERC20 public tokenB;
+    
+    uint256 public reserveA;
+    uint256 public reserveB;
+    
+    // Eventos
+    event LiquidityAdded(uint256 amountA, uint256 amountB);
+    event TokensSwapped(address indexed user, address fromToken, address toToken, uint256 amountIn, uint256 amountOut);
+    event LiquidityRemoved(uint256 amountA, uint256 amountB);
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Solo el owner puede ejecutar");
+        _;
+    }
+    
+    
+    constructor(address _tokenA, address _tokenB) {
+        require(_tokenA != address(0) && _tokenB != address(0), "Direcciones invalidas");
+        owner = msg.sender;
+        tokenA = IERC20(_tokenA);
+        tokenB = IERC20(_tokenB);
+    }
+    
+
+    function addLiquidity(uint256 amountA, uint256 amountB) external onlyOwner nonReentrant {
+        require(amountA > 0 && amountB > 0, "Cantidades deben ser mayor a 0");
+        
+        require(tokenA.transferFrom(msg.sender, address(this), amountA), "Fallo TokenA");
+        require(tokenB.transferFrom(msg.sender, address(this), amountB), "Fallo TokenB");
+        
+        reserveA += amountA;
+        reserveB += amountB;
+        
+        emit LiquidityAdded(amountA, amountB);
+    }
+    
+ 
+    function swapAforB(uint256 amountAIn) external nonReentrant {
+        require(amountAIn > 0, "Cantidad debe ser mayor a 0");
+        require(reserveA > 0 && reserveB > 0, "No hay liquidez disponible");
+ 
+        uint256 amountBOut = (reserveB * amountAIn) / (reserveA + amountAIn);
+        require(amountBOut > 0, "Output insuficiente");
+        require(amountBOut < reserveB, "Liquidez insuficiente");
+        
+        require(tokenA.transferFrom(msg.sender, address(this), amountAIn), "Fallo en ingreso A");
+        require(tokenB.transfer(msg.sender, amountBOut), "Fallo en retiro B");
+        
+        reserveA += amountAIn;
+        reserveB -= amountBOut;
+        
+        emit TokensSwapped(msg.sender, address(tokenA), address(tokenB), amountAIn, amountBOut);
+    }
+    
+
+    function swapBforA(uint256 amountBIn) external nonReentrant {
+        require(amountBIn > 0, "Cantidad debe ser mayor a 0");
+        require(reserveA > 0 && reserveB > 0, "No hay liquidez disponible");
+        
+
+        uint256 amountAOut = (reserveA * amountBIn) / (reserveB + amountBIn);
+        require(amountAOut > 0, "Output insuficiente");
+        require(amountAOut < reserveA, "Liquidez insuficiente");
+        
+        require(tokenB.transferFrom(msg.sender, address(this), amountBIn), "Fallo en ingreso B");
+        require(tokenA.transfer(msg.sender, amountAOut), "Fallo en retiro A");
+        
+        reserveB += amountBIn;
+        reserveA -= amountAOut;
+        
+        emit TokensSwapped(msg.sender, address(tokenB), address(tokenA), amountBIn, amountAOut);
+    }
+    
+  
+    function removeLiquidity(uint256 amountA, uint256 amountB) external onlyOwner nonReentrant {
+        require(amountA > 0 && amountB > 0, "Cantidades deben ser mayor a 0");
+        require(amountA <= reserveA && amountB <= reserveB, "Reservas insuficientes");
+        
+        require(tokenA.transfer(msg.sender, amountA), "Fallo TokenA");
+        require(tokenB.transfer(msg.sender, amountB), "Fallo TokenB");
+        
+        reserveA -= amountA;
+        reserveB -= amountB;
+        
+        emit LiquidityRemoved(amountA, amountB);
+    }
+    
+
+    function getPrice(address _token) external view returns (uint256) {
+        require(_token == address(tokenA) || _token == address(tokenB), "Token no soportado");
+        require(reserveA > 0 && reserveB > 0, "No hay liquidez disponible");
+        
+        if (_token == address(tokenA)) {
+            return (reserveB * 1e18) / reserveA;
+        } else {
+            return (reserveA * 1e18) / reserveB;
+        }
+    }
+    
+  
+    function simulateSwap(address tokenIn, uint256 amountIn) external view returns (uint256 amountOut) {
+        require(tokenIn == address(tokenA) || tokenIn == address(tokenB), "Token invalido");
+        require(amountIn > 0, "Cantidad debe ser mayor a 0");
+        require(reserveA > 0 && reserveB > 0, "No hay liquidez disponible");
+        
+        if (tokenIn == address(tokenA)) {
+            // (reserveA + amountIn)(reserveB - amountOut) = reserveA * reserveB
+            amountOut = (reserveB * amountIn) / (reserveA + amountIn);
+        } else {
+            // (reserveB + amountIn)(reserveA - amountOut) = reserveA * reserveB
+            amountOut = (reserveA * amountIn) / (reserveB + amountIn);
+        }
+    }
+}
